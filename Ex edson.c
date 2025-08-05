c43b010656368da1962a6964a5a9ecee657979d6
felipe783 <felipebolt2007@Gmail.com>
2025-08-05 10:55:44 -0300

    Update Ex edson.c

--- Ex edson.c
+++ Ex edson.c
@@ -5,43 +5,45 @@
 
 int main(){
 	int i,x,aux;
-	float media,mediaP,aux2,aux3;
-	printf("Quantas notas do aluno:");
-	scanf("%i",&aux);
-	float pesos[aux], notas[aux];
-	for(i=0;i<aux;i++){
-		printf("\nFale %iº nota do aluno:",i+1);
-		scanf("%f",&notas[i]);
-		printf("\nFale o peso da nota:");
-		scanf("%f",&pesos[i]);
-		aux3 +=pesos;
-	}
-	//Media
-	for(i=0;i<aux;i++){
-		aux2+=notas[i];
-	}
-	//MediaP
-	float nota2[aux];
-	for(i=0;i<aux;i++){
-		nota2[i]=notas[i]*pesos[i];
-		aux3 +=nota2[i];
-		
-	}
-	media=aux2/aux;
-	mediaP=aux3/aux3;
-	printf("\nMedia= %.2f",media);
-	printf("\nMedia Ponderada= %.2f",mediaP);
-	if(mediaP>=7){
-		printf("\nAprovado");
-	}
-	else{
-		if((mediaP<7)&&(mediaP>=3)){
-			printf("\nExame");
+	float media,mediaP,aux2,aux3,aux4;
+	printf("Quantos alunos:");
+	scanf("%i",&aux4);
+		for(i=0;i<aux4;i++){
+			float pesos[aux], notas[aux];
+			printf("Quatas notas para esse aluno:");
+			scanf("%i",&aux);
+			for(i=0;i<aux;i++){
+				printf("\nFale %iº nota do aluno:",i+1);
+				scanf("%f",&notas[i]);
+				printf("\nFale o peso da nota:");
+				scanf("%f",&pesos[i]);
+				aux3 +=pesos[i];
+			}
+			//Media
+			for(i=0;i<aux;i++){
+				aux2+=notas[i];
+			}
+			//MediaP
+			float nota2[aux];
+			for(i=0;i<aux;i++){
+				nota2[i]=notas[i]*pesos[i];
+				aux3 +=nota2[i];
+			}
+			media=aux2/aux;
+			mediaP=aux3/aux3;
+			printf("\nMedia= %.2f",media);
+			printf("\nMedia Ponderada= %.2f",mediaP);
+			if(mediaP>=7){
+				printf("\nAprovado");
+			}
+			else{
+				if((mediaP<7)&&(mediaP>=3)){
+					printf("\nExame");
+				}
+				else{
+					printf("\nReprovado");
+				}
+			}	
 		}
-		else{
-			printf("\nReprovado");
-		}
 	}
-
-}
 
