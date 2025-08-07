38f3cdcac3f86f12362680d9a9c7700931ee25c0
felipe783 <felipebolt2007@Gmail.com>
2025-08-07 09:12:31 -0300

    Update Ex edson.c
    Sa porra

--- Ex edson.c
+++ Ex edson.c
@@ -4,35 +4,58 @@
 #include <conio.h>
 
 int main(){
-	int i,x,aux;
-	float media,mediaP,aux2,aux3,aux4;
+	int num_alunos,i,tipo,num_notas,aluno;
+	float Soma_Notas=0,mediaF,Soma_Pesos=0,notas2[num_notas],soma_NP=0,mediaP,media;
 	printf("Quantos alunos:");
-	scanf("%i",&aux4);
-		for(i=0;i<aux4;i++){
-			float pesos[aux], notas[aux];
-			printf("Quatas notas para esse aluno:");
-			scanf("%i",&aux);
-			for(i=0;i<aux;i++){
-				printf("\nFale %iº nota do aluno:",i+1);
-				scanf("%f",&notas[i]);
-				printf("\nFale o peso da nota:");
+	scanf("%i",&num_alunos);
+	for(aluno=0;aluno<num_alunos;aluno++){
+		printf("\n Aluno %i \n",aluno+1);
+		printf("Quantas notas:");
+		scanf("%i",&num_notas);
+		//Tamanho vetor(nao é os valores)
+		float nota[num_notas],pesos[num_notas];
+		//
+		printf("\nMedia[1] ou Media Ponderada[2]:");
+		scanf("%i",&tipo);
+		//Leitura de notas
+		for(i=0;i<num_notas;i++){
+			printf("Fale a nota %iº:\n",i+1);
+			scanf("%f",&nota[i]);
+			//Pesos
+			if(tipo==2){
+				printf("Fale o peso:\n");
 				scanf("%f",&pesos[i]);
-				aux3 +=pesos[i];
 			}
-			//Media
-			for(i=0;i<aux;i++){
-				aux2+=notas[i];
+		}
+		//Media
+		if(tipo==1){
+			for(i=0;i<num_notas;i++){
+				Soma_Notas +=nota[i];			
 			}
-			//MediaP
-			float nota2[aux];
-			for(i=0;i<aux;i++){
-				nota2[i]=notas[i]*pesos[i];
-				aux3 +=nota2[i];
+			media=0;
+			media=Soma_Notas/num_notas;
+			printf("\nA media pondera: %.2f\n",media);
+			if(media>=7){
+			printf("\nAprovado");
 			}
-			media=aux2/aux;
-			mediaP=aux3/aux3;
-			printf("\nMedia= %.2f",media);
-			printf("\nMedia Ponderada= %.2f",mediaP);
+			else{
+				if((media<7)&&(media>=3)){
+					printf("\nExame");
+				}
+				else{
+					printf("\nReprovado");
+				}
+			}
+		}
+		//Media Ponderada
+		else{
+			for(i=0;i<num_notas;i++){
+				Soma_Pesos += pesos[i];
+				soma_NP +=nota[i]*pesos[i];
+			}
+			mediaP=0;
+			mediaP=soma_NP/Soma_Pesos;
+			printf("\nA media pondera: %.2f\n",mediaP);
 			if(mediaP>=7){
 				printf("\nAprovado");
 			}
@@ -43,7 +66,7 @@ int main(){
 				else{
 					printf("\nReprovado");
 				}
-			}	
+			}
 		}
 	}
-
+}
