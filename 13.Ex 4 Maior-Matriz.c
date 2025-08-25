433fad269c0fcbb52c75026445eb946cdb2c98f1
felipe783 <felipebolt2007@Gmail.com>
2025-08-25 10:14:48 -0300

    Create 13.Ex 4 Maior-Matriz.c

---
+++ 13.Ex 4 Maior-Matriz.c
@@ -0,0 +1,32 @@
+#include <math.h>
+#include <conio.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+#include <locale.h>
+
+//Soma linhas da matriz
+
+int main(){
+	setlocale(LC_ALL, ""); //pra ficar bonito
+	int matriz[3][3] = {};
+	int i,j;
+	int soma[3],somaTotal;
+	for(i=0;i<3;i++){ //Matriz
+		for(j=0;j<3;j++){
+	 		printf("Fale o numero na posição [%d][%d]:",i,j);
+	 		scanf("%d",&matriz[i][j]);
+	 	}
+	}
+	for(i=0;i<3;i++){ //vetor
+		for(j=0;j<3;j++){
+			somaTotal += matriz[i][j];
+			soma[i] += matriz[i][j];
+		}
+	}
+	printf("\nSoma total %d",somaTotal);
+	for(i=0;i<3;i++){
+		printf("\nSoma da linha [%d]:%d",i,soma[i]);
+	}
+		
+}
