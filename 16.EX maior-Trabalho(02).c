ebb47d2982b8bc2f97535fc18469ed0a02becf26
felipe783 <felipebolt2007@Gmail.com>
2025-09-01 10:38:20 -0300

    Update 16.EX maior-Trabalho(02).c

--- 16.EX maior-Trabalho(02).c
+++ 16.EX maior-Trabalho(02).c
@@ -7,11 +7,17 @@
 por linha. Essa média deve ser armazenada em um vetor de 3 linhas.*/
 
 int main(){
-	float C[3][4]={4.0,8.0,6.0,7.0,3.0,9.0,7.5,8.5,6.5,4.0,7.5,6.0};
+	float C[3][4]={0};
 	int i,j,cont,aluno=4;
 	float media,soma;
 	printf("Quantas notas por aluno:");
 	scanf("%d",&aluno);
+	for(i=0;i<3i++){
+		for(j=0;j<4;j++){
+			printf("\n Fale o numero na posicao [%d] [%d]:",i,j);
+			scanf("%f",&C[i][j]);
+		}
+	}
 	//Entrada de dados
 	printf("Insira os valores para a matriz 3x4: \n" );
 	for (i=0;i<3;i++){
