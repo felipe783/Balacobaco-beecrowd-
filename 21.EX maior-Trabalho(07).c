d34a31312516708da1b440b52053360167578acd
felipe783 <felipebolt2007@Gmail.com>
2025-09-01 18:16:05 -0300

    Create 21.EX maior-Trabalho(07).c

---
+++ 21.EX maior-Trabalho(07).c
@@ -0,0 +1,35 @@
+#include <math.h>
+#include <conio.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+#include <locale.h>
+
+int main(){
+    int i,j;
+    // i é linha e j coluna
+	int linha,coluna;
+	float soma[i];
+	//Definir a dimensão matriz
+	printf("\nFale a dimensao da linha:");
+	scanf("%d",&linha);
+	printf("\nFale a dimensao da coluna:");
+	scanf("%d",&coluna);
+    int A[linha][coluna];
+    // Deixa o vetor com zero tudo
+    for(j = 0; j < coluna; j++){
+        soma[j] = 0;
+    }
+    //Dados
+    for(i=0;i<linha;i++){
+        for(j=0;j<coluna;j++){ //Coluna
+            printf("Fale o valor na posicao[%d][%d]:",i,j);
+            scanf("%d",&A[i][j]);
+            soma[j] += A[i][j];    
+        }
+    } 
+    for(j=0;j<linha;j++){
+        printf("\nA soma dos elementos da coluna %d e:[%.2f]\n",j,soma[j]);
+    }
+    return 0;
+}
