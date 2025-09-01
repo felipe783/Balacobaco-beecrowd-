1565aae2d339f9046cce9697925d783b12ddaaa0
felipe783 <felipebolt2007@Gmail.com>
2025-09-01 18:22:51 -0300

    Create 22.EX maior-Trabalho(08).c

---
+++ 22.EX maior-Trabalho(08).c
@@ -0,0 +1,15 @@
+#include <math.h>
+#include <conio.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+#include <locale.h>
+
+/*Faça um algoritmo que monte uma matriz com as dimensões do usuário e crie
+um vetor de duas posições onde a soma dos pares deve ser inserida na
+posição zero do vetor e a soma dos impares na posição um do vetor.*/
+
+int main(){
+    
+    return 0;
+}
